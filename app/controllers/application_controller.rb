a4809f47b721adbf9bb826174c37875e8cf9c2e4
Mauro Ponce <mgp1989@gmail.com>
2019-03-05 23:57:22 -0600

    Add User model and setup JWT with login and logout endpoints

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::API
+  include ::ActionController::Cookies
+  before_action :authenticate
+
+  def authenticate
+    jwt = cookies.signed[:jwt]
+    user_id = JsonWebToken.decode(jwt)[:user_id]
+    @current_user = User.find(user_id)
+
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { errors: e.message }, status: :unauthorized
+    rescue JWT::DecodeError => e
+      render json: { errors: e.message }, status: :unauthorized
+  end
+
+  protected
+    def current_user
+      @current_user
+    end
 end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+  skip_before_action :authenticate
+
+  def create
+    user = User.find_by(email: login_params[:email])
+    if user.authenticate(login_params[:password])
+      jwt = JsonWebToken.encode({user_id: user.id})
+      cookies.signed[:jwt] = {
+        value:  jwt,
+        httponly: true
+      }
+      render json: user
+    else
+      render json: { error: 'Username or password incorrect'},
+        status: :unauthorized
+    end
+  end
+
+  def destroy
+    cookies.delete(:jwt)
+  end
+
+  private
+    def login_params
+      params.permit(:email, :password)
+    end
+end
